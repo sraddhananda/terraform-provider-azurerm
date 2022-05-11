173932811adab39c5c7b7aa134bfb4c1caa6497c
sraddhananda <34411941+sraddhananda@users.noreply.github.com>
2022-05-11 10:40:32 +0100

    Update resource_timeout.go

--- vendor/github.com/hashicorp/terraform-plugin-sdk/helper/schema/resource_timeout.go
+++ vendor/github.com/hashicorp/terraform-plugin-sdk/helper/schema/resource_timeout.go
@@ -14,11 +14,11 @@ const TimeoutKey = "e2bfb730-ecaa-11e6-8f88-34363bc7c4c0"
 const TimeoutsConfigKey = "timeouts"
 
 const (
-	TimeoutCreate  = "create"
-	TimeoutRead    = "read"
-	TimeoutUpdate  = "update"
-	TimeoutDelete  = "delete"
-	TimeoutDefault = "default"
+	TimeoutCreate  = "Create"
+	TimeoutRead    = "Read"
+	TimeoutUpdate  = "Update"
+	TimeoutDelete  = "Delete"
+	TimeoutDefault = "Default"
 )
 
 func timeoutKeys() []string {
